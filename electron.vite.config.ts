72558e2e2cc758f90c44c4e0586f7aa39d8d6cf7
zhengzhong <yczhengzhong@163.com>
2022-07-17 00:29:40 +0800

    init

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -1,6 +1,6 @@
-import { resolve } from 'path'
-import { defineConfig } from 'electron-vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
+import { defineConfig } from 'electron-vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   main: {
@@ -23,6 +23,21 @@ export default defineConfig({
         '@renderer': resolve('src/renderer/src')
       }
     },
-    plugins: [react()]
+    plugins: [react()],
+    css: {
+      //* css模块化
+      modules: {
+        // css模块化 文件以.module.[css|less|scss]结尾
+        generateScopedName: '[folder]__[local]___[hash:base64:5]',
+        hashPrefix: 'quickapp'
+      },
+      //* 预编译支持less
+      preprocessorOptions: {
+        less: {
+          // 支持内联 JavaScript
+          javascriptEnabled: true
+        }
+      }
+    }
   }
-})
+});
--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import styles from '../assets/index.module.less';
 
 function Versions(): JSX.Element {
-  const [versions] = useState(window.electron.process.versions)
+  const [versions] = useState(window.electron.process.versions);
 
   return (
-    <ul className="versions">
+    <ul className={styles.versions}>
       <li className="electron-version">Electron v{versions.electron}</li>
       <li className="chrome-version">Chromium v{versions.chrome}</li>
       <li className="node-version">Node v{versions.electron}</li>
       <li className="v8-version">V8 v{versions.node}</li>
     </ul>
-  )
+  );
 }
 
-export default Versions
+export default Versions;
