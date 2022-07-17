e0a48e2cb0566e2f7fe1e055ebc29d9fc267bc0c
zhengzhong <yczhengzhong@163.com>
2022-07-17 22:47:11 +0800

    init

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,13 @@
 import styles from './index.module.less';
-import { Layout } from 'antd';
+import { Layout, Space, Input, Button } from 'antd';
+import { createFromIconfontCN } from '@ant-design/icons';
 import React from 'react';
+import cn from 'classnames';
 
 const { Header, Footer, Sider, Content } = Layout;
+const IconFont = createFromIconfontCN({
+  scriptUrl: ['/src/assets/iconfont.js']
+});
 
 interface AppProps {}
 
@@ -10,17 +15,76 @@ function App(props: AppProps) {
   return (
     <div className={styles.container}>
       <Layout>
-        <Sider className={styles.left} width={'48px'}>
-          Sider
-        </Sider>
+        <Header className={styles.head}>
+          <div className={styles.headLeft}>
+            <IconFont type="icon-quickapp-apple" />
+          </div>
+          <div className={styles.headCenter}>
+            <Space size={'small'}>
+              <div className={cn(styles.headIcon, styles.selected)}>
+                <IconFont type="icon-quickapp-computer" />
+              </div>
+              <div className={styles.headIcon}>
+                <IconFont type="icon-quickapp-pad" />
+              </div>
+              <div className={styles.headIcon}>
+                <IconFont type="icon-quickapp-Mobile-" />
+              </div>
+              <div className={styles.headForm}>
+                <Input size={'small'} addonAfter="px" style={{ width: '100px' }} />
+              </div>
+            </Space>
+            <div style={{ marginLeft: '40px' }}>
+              <Space size={'small'}>
+                <div className={styles.headIcon}>
+                  <IconFont type="icon-quickapp-hotui" />
+                </div>
+                <div className={styles.headIcon}>
+                  <IconFont type="icon-quickapp-qianjin" />
+                </div>
+              </Space>
+            </div>
+          </div>
+          <div className={styles.headRight}>
+            <Space size={'small'}>
+              <Button size={'middle'} type={'primary'}>
+                保存
+              </Button>
+              <Button size={'middle'}>预览</Button>
+            </Space>
+          </div>
+        </Header>
         <Layout>
-          <Header className={styles.head}>Header</Header>
+          <Sider className={styles.left} width={'48px'}>
+            <div className={styles.leftContent}>
+              <div className={styles.leftStart}>
+                <Space size={'middle'} direction={'vertical'}>
+                  <div className={styles.leftIcon}>
+                    <IconFont type="icon-quickapp-xiangmu" />
+                  </div>
+                  <div className={styles.leftIcon}>
+                    <IconFont type="icon-quickapp-component" />
+                  </div>
+                </Space>
+              </div>
+              <div className={styles.leftEnd}>
+                <Space size={'middle'} direction={'vertical'}>
+                  <div className={styles.leftIcon}>
+                    <IconFont type="icon-quickapp-yonghuxieyi" />
+                  </div>
+                </Space>
+              </div>
+            </div>
+          </Sider>
           <Content className={styles.content}>
             <div className={styles.frame}>
               <div className={styles.body}></div>
             </div>
+            <div className={styles.footer}>QuickApp ©2022 Created by zhengzhong</div>
           </Content>
-          <Footer className={styles.footer}>QuickApp ©2022 Created by zhengzhong</Footer>
+          <Sider className={styles.right} width={'300px'}>
+            请选择节点
+          </Sider>
         </Layout>
       </Layout>
     </div>
